689cd964b7dfb60459ad7d4d12c56f0f0d153805
joel cronin <you@example.com>
2022-05-20 17:59:16 -0400

    basic Flexbox and Start button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+// Variable targeting HTML Elements
 var timerEl = document.getElementById('timer');
 var boxEl = document.getElementById("box");
 var highScoreEl = document.getElementById('highscores');
+var startButton =document.getElementById('startButton')
 
+// Question and answer objects
 var question1 = {
     question: 'What can a string consist of?',
     wrongAnswers: ["letters, numbers, and/or symbols", "a piece of twine", "Just letters"],
@@ -13,6 +16,12 @@ var question2 = {
     wrongAnswers: ["Nice and Neat", "Not a New", "Nimble apple Note"],
     rightAnswer: "Not a Number"
 };
+
+//Standard Variables
+var isWin = false;
+var winTally = 0;
+var lossTally = 0;
+var scoreTally = 0;
 
 function countDown() {
     var timeLeft = 60;
@@ -28,4 +37,11 @@ function countDown() {
         }
     }, 1000)
 }
-countDown();
+
+function startQuiz(){
+    isWin = false
+    countDown()
+    //Add function to produce first question box
+}
+
+startButton.addEventListener("click", startQuiz);
