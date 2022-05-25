1457b4099ff9790b955f1e1adcd785baa1263a83
joel cronin <you@example.com>
2022-05-24 20:47:25 -0400

    CSS styling finalised and comments added in JS and HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,17 @@
 var timerEl = document.getElementById('timer');
 var boxEl = document.getElementById("box");
 var highScoreEl = document.getElementById('highScores');
+var buttonContainer = document.getElementById('buttonContainer');
 var startButton = document.getElementById('startButton');
 var questionBox = document.getElementById('questionBox');
 var answerBox1 = document.getElementById('answerBox1');
 var answerBox2 = document.getElementById('answerBox2');
 var answerBox3 = document.getElementById('answerBox3');
 var commentBox = document.getElementById('commentBox');
+var finalBox = document.getElementById("finalBox")
 var currentScore = document.getElementById('currentScore');
 var finalScore = document.getElementById('finalScore')
-var finalScore2 = document.getElementById('finalScore2')
-var allAnswerBoxes = document.querySelectorAll('p')
 var container = document.getElementById("quizContainer")
-var scoreDisplay = document.getElementById("finalBox")
 var initialsEl = document.getElementById("initialsBox")
 var submitButton = document.getElementById("submit")
 var scoreDisplay = document.getElementById("scoreDisplay")
@@ -24,12 +23,12 @@ var liEl2 = document.getElementById("li2")
 var liEl3 = document.getElementById("li3")
 var liEl4 = document.getElementById("li4")
 
-// Question and answer objects
+// Question and answer objects. Program will still work if new object with same format is added
 var questions = [
     { 
     question: 'What can a string consist of?',
-    answers: ["letters, numbers, and/or symbols","a piece of twine", "Just letters"],
-    rightAnswer: "letters, numbers, and/or symbols"
+    answers: ["letters, numbers, and symbols","a piece of twine", "Just letters"],
+    rightAnswer: "letters, numbers, and symbols"
     },
     {
     question: 'What does NaN stand for',
@@ -42,68 +41,24 @@ var questions = [
     rightAnswer: "True"
     },
     {
-    question: 'What is a for loop used for?',
-    answers: ["To count up to 100 and back down again", "To cycle between functions", "Check for a condition and then repeatedly execute a block of code whilst conditions are met"],
-    rightAnswer: "Check for a condition and then repeatedly execute a block of code whilst conditions are met"
+    question: 'Where is information stored in JavaScript?',
+    answers: ["In Methods", "In for Loops", "In Variables"],
+    rightAnswer: "In Variables"
 }];
  
-// liEl0.textContent = localStorage.getItem("initialsValue") + " scored: " + localStorage.getItem("scoreTally")
-// liEl0.textContent = JSON.parse(localStorage.getItem("scoreArray"))
-
-
 //Standard Variables
-var isWin = false;
-var winTally = 0;
-var lossTally = 0;
 var scoreTally = 0;
 var timeLeft = 20;
 var questionIndex = 0;
-var highScoreIndex = 0;
-var totalScore = 0;
 var initialsValue = 0;
 var scoreObject = {};
 var scoreArray = [];
 
 
-// scoreArray = JSON.parse(localStorage.getItem("scoreArray"));
-
-//THe code above can only run once there is something in local storage, before that wont work and drops everything out
-
-
-// liEl0.textContent = scoreArray[0].name + " scored: " + scoreArray[0].score;
-// liEl1.textContent = scoreArray[1].name + " scored: " + scoreArray[1].score;
-// liEl2.textContent = scoreArray[2].name + " scored: " + scoreArray[2].score;
-// liEl3.textContent = scoreArray[3].name + " scored: " + scoreArray[3].score;
-
-// // liEl0.textContent = Object.keys(scoreArray[0]) + " scored: " + scoreArray[0].property;
-
-// function displayHS (){
-
-//     if(parseScore=== null){
-//         liEl0.textContent = ''
-// } else {
-//     liEl0.textContent = liEl0.textContent = scoreArray[0].name + " scored: " + scoreArray[0].score;
-// }
-// }
-// {
-// liEl0.textContent = scoreArray[0].name + " scored: " + scoreArray[0].score
-// } else if (scoreArray.length === 2) {
-//     liEl1.textContent = scoreArray[1].name + " scored: " + scoreArray[1].score
-// } else if (scoreArray.length === 3) {
-//     liEl2.textContent = scoreArray[2].name + " scored: " + scoreArray[2].score
-// }else if (scoreArray.length === 4) {
-//     liEl3.textContent = scoreArray[3].name + " scored: " + scoreArray[3].score
-// } else {
-//     liEl4.textContent = "Scoreboard Full"
-// }
-// }
-// liEl0.textContent = scoreArray.name[0] + " scored: " + scoreArray.score[0]
-
 //Will start quiz when button is presed
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz(){
-    // isWin = false
     countDown();
     generateQuestion();
     startButton.setAttribute("style", "display: none");
@@ -112,22 +67,21 @@ function startQuiz(){
     setScore();
     container.addEventListener("click", evaluateAnswer)
 };
-
+//Begins and controls the timing of the quiz
 function countDown() {
     
     var timeInterval = setInterval( function(){
         if (timeLeft > 0) {
-            timerEl.textContent = timeLeft + "⌛";
+            timerEl.textContent = "⌛" + timeLeft;
             timeLeft--;
         } else {
-            timerEl.setAttribute("style", "display: none");
-            clearInterval(timeInterval);
-            localStorage.setItem("totalScore", totalScore);
             showScore();
+            clearInterval(timeInterval);
         }
     }, 1000)
 }
 
+//Populates the questions to the relevant place in the HTML
 function generateQuestion () {
     questionBox.textContent = questions[questionIndex].question;
     answerBox1.textContent = questions[questionIndex].answers[0];
@@ -135,6 +89,7 @@ function generateQuestion () {
     answerBox3.textContent = questions[questionIndex].answers[2];
 };
 
+//CHecks if answer is right or wrong and then reacts to this
 function evaluateAnswer (event) {
     var element = event.target;
 
@@ -154,86 +109,71 @@ if (element.matches(".answer") && element.textContent === questions[questionInde
 }
 }
 
+//Keeps track of score
 function setScore() {
     highScoreEl.textContent = " Current Score: " + scoreTally
  }
 
+//Monitors how many questions have been asked and then brings up final score once questions are complete
 function questionCycle () {
     if (questionIndex < questions.length){
         generateQuestion ();
     } else {
         container.removeEventListener("click", evaluateAnswer)
-        // var totalScore = timeLeft + scoreTally
-        // localStorage.setItem("totalScore", totalScore);
         showScore();
     }
 }
 
+//Changes display to show and give option to submit scores
 function showScore() {
     container.setAttribute("style", "display: none");
     timerEl.setAttribute("style", "display: none");
     finalScore.textContent = "QUIZ IS OVER!!!! Your final Score was " + scoreTally + "! If you want to register your Score please put your initials below.";
     highScoreEl.setAttribute("style", "display: none");
     initialsEl.setAttribute("style", "display: block");
+    finalBox.setAttribute("style", "height: 150px", "margin-top: 20px")
+    buttonContainer.setAttribute("style", "display: none");
    }
 
+//If user chooses to log score then this function pushes initials and score into an array that is saved in local storage where it can be used to populate High Scores
 submitButton.addEventListener("click", function(event){
     event.preventDefault();
     scoreArray = JSON.parse(localStorage.getItem("scoreArray")|| "[]")
     var initialsValue = document.getElementById("initials").value;
     scoreObject = {name: initialsValue, score: scoreTally}
     scoreArray.push(scoreObject);
+    submitButton.setAttribute("style", "display: none");
 
     if(initialsValue === "") {
         scoreDisplay.textContent = "Initials cannot be blank!"
     } else {
-        localStorage.setItem("initialsValue", initialsValue);
-        localStorage.setItem("scoreTally", scoreTally);
         localStorage.setItem("scoreArray", JSON.stringify (scoreArray));
         scoreArray = JSON.parse(localStorage.getItem("scoreArray"));
-        // localStorage.setItem("scoreTally", JSON.stringify (scoreTally));
         scoreArray.push(scoreObject);
         renderHighScore();
     }
 })
 
+//Renders Scores to the Recent scores section. Will display the last five scores only as it utilises minus numbers in the index
 function renderHighScore (){
-    var initialsValue = localStorage.getItem("initialsValue")
-    var scoreTally = localStorage.getItem("scoreTally")
-    // scoreObject = {name: initialsValue.value, score: scoreTally.value}
-    // scoreArray.push(scoreObject);
-    // localStorage.setItem("scoreArray", JSON.stringify(scoreArray));
     var parseScore = JSON.parse(localStorage.getItem("scoreArray"));
     if (parseScore !== null) {
-        liEl0.textContent = parseScore[0].name + parseScore[0].score;
-        liEl1.textContent = parseScore[1].name + parseScore[1].score;    
+        liEl0.textContent = parseScore[parseScore.length -1].name + " Scored " + parseScore[parseScore.length -1].score;
+        liEl1.textContent = parseScore[parseScore.length-2].name + " Scored " + parseScore[parseScore.length-2].score;
+        liEl2.textContent = parseScore[parseScore.length-3].name + " Scored " + parseScore[parseScore.length-3].score; 
+        liEl3.textContent = parseScore[parseScore.length-4].name + " Scored " + parseScore[parseScore.length-4].score;
+        liEl4.textContent = parseScore[parseScore.length-5].name + " Scored " + parseScore[parseScore.length-5].score;   
     }
 }
 
+//Allows user to play quiz again
 function playAgain () {
     startButton.setAttribute("style", "display: block");
     initialsEl.setAttribute("style", "display: hidden");
-    // // scoreTally = 0;
-    highScoreIndex++;
     scoreArray = JSON.parse(localStorage.getItem("scoreArray"));
-    // scoreArray = localStorage.getItem("scoretally")
-    // liEl0.textContent = localStorage.getItem("initialsValue") + " scored: " + localStorage.getItem("scoreTally")
 }
-
+//Event listener for play again button
 playAgainButton.addEventListener("click", playAgain)
 
-sortHighScores = (a,b) => {
-    return a.score - b.score;
-}
-
-keepToFive = () => {
-    if(scoreArray.length === 6){
-        scoreArray.pop();
-    }
-}
-
-console.log(scoreObject)
-console.log(scoreArray)
-// displayHS();
-// console.log(highScoreIndex)
+//Ensures Recent Scores is populated on loading of page
 renderHighScore()
