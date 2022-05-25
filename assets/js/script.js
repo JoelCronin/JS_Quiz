66c1c701cfc8060bf91f5400056b739422c70991
joel cronin <you@example.com>
2022-05-25 17:37:03 -0400

    HTML Changes to opening window

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ var container = document.getElementById("quizContainer")
 var initialsEl = document.getElementById("initialsBox")
 var submitButton = document.getElementById("submit")
 var scoreDisplay = document.getElementById("scoreDisplay")
+var playAgainButton = document.getElementById("playAgain")
 var liEl0 = document.getElementById("li0")
 var liEl1 = document.getElementById("li1")
 var liEl2 = document.getElementById("li2")
@@ -153,6 +154,7 @@ submitButton.addEventListener("click", function(event){
     }
 })
 
+
 //Renders Scores to the Recent scores section. Will display the last five scores only as it utilises minus numbers in the index
 function renderHighScore (){
     var parseScore = JSON.parse(localStorage.getItem("scoreArray"));
@@ -164,6 +166,7 @@ function renderHighScore (){
         liEl4.textContent = parseScore[parseScore.length-5].name + " Scored " + parseScore[parseScore.length-5].score;   
     }
 }
+
 
 
 //Ensures Recent Scores is populated on loading of page
